efb5576d9f9011afeb3516ddc2eda2bc2cb29dd1
Sally Rain <91876911+sallyrain@users.noreply.github.com>
2021-12-20 10:11:21 +0300

    Update t1.cpp

--- Homework/25.10.21/t1.cpp
+++ Homework/25.10.21/t1.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int day, month, year;
-    do {
-        cout << "Input day: ";
-        cin >> day;
-    } while (day < 1 or day > 31);
-    do {
-        cout << "Input month: ";
-        cin >> month;
-    } while (month < 1 or month > 12);
-    cout << "Input year: ";
-    cin >> year;
-    int a = (14 - month) / 12;
-    int y = year - a + 1;
-    int m = month + 12 * a - 2;
-    int d = 0;
-    while (1) {
-        d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
-        if (d == 5) break;
-        y += 1;
-    }
-    cout << y;
-    return 0;
+	int day;
+	int month;
+	int year;
+	do {
+		cout << "Enter day: " << endl;
+		cin >> day;
+	} while (day < 1 || day>31);
+	do {
+		cout << "Enter month: " << endl;
+		cin >> month;
+	} while (month < 1 || month>12);
+	do {
+		cout << "Enter year: " << endl;
+		cin >> year;
+	} while (year < 1);
+	int a = (14 - month) / 12;
+	int m = month + 12 * a - 2;
+	int d = 0;
+	while (d != 4) {
+		int y = year - a;
+		d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+		year++;
+	}
+	cout << "Next year: " << year;
 }
+Corrected
